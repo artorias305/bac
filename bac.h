29503df21eb2ca9366793bdafca76f90edbfcff3
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-08-09 00:15:26 -0300

    Add header file

---
+++ bac.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_help();
+int char_to_digit(char c);
+int str_to_int(const char *str);
+int convert_to_10(char *num, int base);
+char *convert_from_10(int num, int base);
